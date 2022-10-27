8de0f2de4aba6477d494442644ba4a3fa7fbffa6
luiz-davi <luizd4398@gmail.com>
2022-10-27 08:53:47 -0300

    adicionando historic no seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+HistoricoProfissional.find_or_create_by(
+  funcao: "HelpDesk",
+  regime: "Estágio",
+  empresa: "Zerone Sistemas",
+  cidade: "Garanhuns",
+  estado: "Pernambuco",
+  inicio: "abr. 2021",
+  fim: "fev. 2022"
+)
+
+HistoricoProfissional.find_or_create_by(
+  id: 2,
+  funcao: "Engenheiro de Software",
+  regime: "Estágio",
+  empresa: "Tracking Trade",
+  cidade: "São Paulo",
+  estado: "São Paulo",
+  inicio: "fev. 2022",
+  fim: "Atualmente"
+)
